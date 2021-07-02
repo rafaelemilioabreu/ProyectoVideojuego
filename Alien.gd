c9ff4a2af33518166420d802bb9ab0eddcd56411
rafael <rafaelemilioabreuvaldez@gmail.com>
2021-07-01 20:30:31 -0400

    disparos agregados

--- Alien.gd
+++ Alien.gd
@@ -1,12 +1,29 @@
 extends Area2D
 
+export (PackedScene) var laser_escena
+
+
 export var velocidad = 400
+
+var plLaser = preload("res://Laser.tscn")
+
+
 
 func _process(delta):
 	var direccion = Vector2.ZERO
+	
 	if Input.is_action_pressed("ui_up"):
 		direccion.y -= 1
 	if Input.is_action_pressed("ui_down"):
 		direccion.y += 1
+	if Input.is_action_just_pressed("ui_accept"):
+		var laser = plLaser.instance()
+		laser.position = position
+		get_tree().current_scene.add_child(laser)
+	
+		
+	
 
 	position += direccion * velocidad * delta
+	
+	
