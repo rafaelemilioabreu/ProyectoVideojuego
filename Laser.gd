5bcefad03c79d5e604c1050564486b61cce9a293
rafael <rafaelemilioabreuvaldez@gmail.com>
2021-07-15 22:03:14 -0400

    damage agregado

--- Laser.gd
+++ Laser.gd
@@ -5,3 +5,14 @@ export (float) var velocidad = 500
 
 func _physics_process(delta):
 	position.x -= velocidad * delta
+	
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_Laser_area_entered(area):
+	if area.is_in_group("damageable"):
+		area.damage()
+		queue_free()
