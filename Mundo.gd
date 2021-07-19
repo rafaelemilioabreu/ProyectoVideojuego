d7f20049acfaf38ffaa8f7a9ad5b667a1a21d2f7
rafael <rafaelemilioabreuvaldez@gmail.com>
2021-07-19 16:10:10 -0400

    primera fase del score y hud agregado

--- Mundo.gd
+++ Mundo.gd
@@ -2,6 +2,8 @@ extends Node
 
 export (PackedScene) var globo_escena
 
+var score = 10
+
 
 func _ready():
 	randomize()
@@ -15,8 +17,22 @@ func _on_GloboTimer_timeout():
 	
 	globo.position = globo_spawn_locacion.position
 	
+
+
+func new_game():
+	score = 10
+	$ScoreTimer.start()
+	
+	
+func game_over():
+	$ScoreTimer.stop()
 	
 	
 	
 	
+
+
+func _on_ScoreTimer_timeout():
+	score-=5
+	$HUD.update_score(score)
 	
